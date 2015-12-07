acf1b69fc7324340cb1fffb4924c5f2e1b81ced0
Marc DM <marcdm@fraser>
2015-12-07 14:18:39 -0500

    bugfix for saving nursing component without height+weight

--- components/nursing.py
+++ components/nursing.py
@@ -70,8 +70,20 @@ class EncounterAnthro(BaseComponent):
         return ['weight', 'height', 'hip', 'abdominal_circ']
 
     def make_critical_info(self):
-        citxt = ['%5.2f' % self.weight, 'kg', 'x', '%5.1f' % self.height, 'cm',
-                 '=', '(BMI) %5.2f' % self.bmi]
+        if self.weight and self.height:
+            citxt = ['%5.2f' % self.weight, 'kg', 'x',
+                     '%5.1f' % self.height, 'cm',
+                     '=', '(BMI) %5.2f' % self.bmi]
+        else:
+            citxt = []
+            if self.abdominal_circ:
+                citxt.append('Waist: %5.2fcm' % self.abdominal_circ)
+            if self.hip:
+                if self.abdominal_circ:
+                    citxt.append('x')
+                citxt.append('Hip: %5.2fcm' % self.hip)
+            if self.whr:
+                citxt.append('= %5.2f' % self.whr)
         # return a single line, no more than 140 chars to describe the details
         # of what's happening in the measurements in this component
         return ' '.join(citxt)
