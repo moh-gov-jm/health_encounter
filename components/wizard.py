47c46a7bff69e9c47a1529b7427a4866d4850485
Marc Murray <marcdm@nyzam>
2015-12-16 13:25:10 -0500

    EditComponentWizard rewired to fix multi ppl edit bug
    Originally the 'component' viewState was added to the wizard at the
    time when the wizard was instantiated. This is bad because on the
    wizard, self.states is a class attribute.
    This fix, creates a state for each existing active component when
    the wizard is __setup__ in the trytond engine. When a component
    is selected, the appropriate state is shown based on the type
    of the component. States are named according to the shortname
    of the component shown in the first column.
    
    And bump version number for this bugfix.

--- components/wizard.py
+++ components/wizard.py
@@ -38,6 +38,27 @@ def model2dict(record, fields=None, with_one2many=True):
     return out
 
 
+def get_component_modelview(component_type_id, model_name=None):
+        if component_type_id:
+            # mvd = model view data
+            mvd = EncounterComponentType.read(
+                [int(component_type_id)], fields_names=['model', 'view_form'])
+        elif model_name:
+            mvd = EncounterComponentType.search_read(
+                [('model', '=', model_name)],
+                fields_names=['model', 'view_form'])
+        else:
+            mvd = []
+
+        try:
+            return (mvd[0]['model'], mvd[0]['view_form'])
+        except IndexError:  # there is no component type for this model
+            raise UnknownEncounterComponentType(model_name)
+
+txtonly = re.compile('([A-Za-z]+)')
+name_fix = lambda x: '_'.join([p.lower() for p in txtonly.findall(x)])
+
+
 class ChooseComponentTypeView(ModelView):
     'Choose Component'
     __name__ = 'gnuhealth.encounter.component_chooser'
@@ -48,13 +69,16 @@ class ChooseComponentTypeView(ModelView):
     @classmethod
     def component_type_selection(cls):
         triple = EncounterComponentType.get_selection_list()
-        return [x[:2] for x in triple]
+        return [(name_fix(x[2]), x[1]) for x in triple]
 
 
 class ComponentStateView(StateView):
     '''use this StateView to get the boilerplate that sets up the
     appropriate instance of the component for creation or editing.'''
-    def __init__(self, model_name, view_id):
+    def __init__(self, component_type_id=None, model_name=None):
+        self.current_model_view = (
+            'gnuhealth.encounter.component',
+            'health_encounter.health_view_form_encounter_component')
         buttons = [
             Button('Si_gn', 'sign_x', 'health-certify', states={
                    'invisible': Bool(Eval('signed_by'))}),
@@ -65,7 +89,14 @@ class ComponentStateView(StateView):
             Button('Close', 'close_x', 'tryton-close',
                    states={'invisible': Not(Bool(Eval('signed_by')))})
         ]
-        super(ComponentStateView, self).__init__(model_name, view_id, buttons)
+
+        # launch me with the combined model and associated view
+        super(ComponentStateView, self).__init__(
+            'gnuhealth.encounter.component',
+            'health_encounter.health_view_form_encounter_component', buttons)
+
+        if component_type_id or model_name:
+            self.set_modelview(component_type_id, model_name)
 
     def get_defaults(self, wiz, state_name, fields):
         _real_comp_data = wiz._component_data.get('obj_data', False)
@@ -75,12 +106,26 @@ class ComponentStateView(StateView):
             _real_comp = wiz._component_data['obj']
             return model2dict(_real_comp, fields)
 
+    def _get_model(self):
+        return self.current_model_view[0]
 
-class CStateView(ComponentStateView):  # initialise a StateView from DB
-    def __init__(self, component_type_id):
-        mvd = EncounterComponentType.read([int(component_type_id)],
-                                          fields_names=['model', 'view_form'])
-        super(CStateView, self).__init__(mvd[0]['model'], mvd[0]['view_form'])
+    def _set_model(self, val):
+        # ignores the value this for the sake of the inherited __init__ call
+        pass
+
+    def _get_view_id(self):
+        return self.current_model_view[1]
+
+    def _set_view_id(self, val):
+        # ignores the value this for the sake of the inherited __init__ call
+        pass
+    model_name = property(_get_model, _set_model)
+    view = property(_get_view_id, _set_view_id)
+
+    def set_modelview(self, type_id, model_name=None):
+        tact = Transaction()
+        print "Fetching model, transaction.context = %s" % repr(tact.context)
+        self.current_model_view = get_component_modelview(type_id, model_name)
 
 
 class EditComponentWizard(Wizard):
@@ -98,75 +143,74 @@ class EditComponentWizard(Wizard):
     save_x = StateTransition()
     sign_x = StateTransition()
     close_x = StateTransition()
-    _component_data = {}
+
+    @classmethod
+    def __setup__(cls):
+        super(EditComponentWizard, cls).__setup__()
+        # fetch the component types and create a ComponentStateView for each
+        component_types = [(x[0], name_fix(x[2]), x[3]) for x in
+                           EncounterComponentType.get_selection_list()]
+
+        cls._component_model_map = {}
+        for type_id, type_name, type_model in component_types:
+            setattr(cls, type_name, ComponentStateView(type_id))
+            cls._component_model_map[type_model] = type_name
 
     def __init__(self, sessionid):
+        super(EditComponentWizard, self).__init__(sessionid)
         tact = Transaction()
         active_model = tact.context.get('active_model')
         active_id = tact.context.get('active_id')
-        txtonly = re.compile('([A-Za-z]+)')
-        fix = lambda x: '_'.join([p.lower() for p in txtonly.findall(x)])
-        component_types = dict([(x[0], fix(x[2])) for x in
-                                EncounterComponentType.get_selection_list()])
 
         self._component_data = {'model': active_model, 'active_id': active_id,
-                                'typenames': component_types,
-                                'obj_data': None}
+                                'obj_data': None, 'selected_component': None}
         real_component = None
-        if active_model != 'gnuhealth.encounter':
+        if active_model != 'gnuhealth.encounter':  # open button was clicked
             real_component = EncounterComponent.union_unshard(active_id)
-            self._make_component_state_view(False, real_component.__name__)
-            self._component_data['obj'] = real_component
-        super(EditComponentWizard, self).__init__(sessionid)
-        # setattr(self, statename, real_component)
-
-    def _make_component_state_view(self, component_type_id, model_name=None):
-        component_view = None
-
-        if not component_type_id and model_name:
-            cvm = EncounterComponentType.search([('model', '=', model_name)])
             try:
-                component_type_id = cvm[0]
-            except IndexError:  # there is no component type for this model
-                raise UnknownEncounterComponentType(model_name)
-
-        # if 'component' not in self.states:
-        component_view = CStateView(component_type_id)
-        self.states['component'] = component_view
-        return 'component'
+                sc = self._component_model_map[real_component.__name__]
+            except IndexError:
+                raise UnknownEncounterComponentType(real_component.__name__)
+                sc = None
+            self._component_data['selected_component'] = sc
+            self._component_data['obj'] = real_component
+        elif getattr(self.selector, 'component_type', False):
+            # maybe we came here via the add component and now we're saving huh
+            self._component_data[
+                'selected_component'] = self.selector.component_type
 
     def transition_start(self):
         if self._component_data['model'] == 'gnuhealth.encounter':
             return 'selector'
         else:
-            return 'component'
+            return self._component_data['selected_component']
 
     def transition_selected(self):
-        statename = self._make_component_state_view(
-            self.selector.component_type)
-        state = self.states[statename]
-        ComponentModel = Pool().get(state.model_name)
+        state_name = self.selector.component_type
+        state = self.states[state_name]
+        component_model = Pool().get(state.model_name)
         encounter_id = self._component_data['active_id']
         component_data = {'encounter': encounter_id}
         view = state.get_view()
         field_names = view['fields'].keys()
         if 'id' in field_names:
             del field_names[field_names.index['id']]
-        component_data.update(ComponentModel.default_get(field_names))
-        real_component = ComponentModel(**component_data)
-        setattr(self, statename, real_component)
+        component_data.update(component_model.default_get(field_names))
+        real_component = component_model(**component_data)
+        setattr(self, 'component', real_component)
         self._component_data.update(obj=real_component,
-                                    obj_data=component_data)
-        return statename
+                                    obj_data=component_data,
+                                    selected_component=state_name)
+        return state_name
 
     def transition_sign_x(self):
-        state_name = 'component'
+        state_name = self._component_data['selected_component']
         state_model = getattr(self, state_name)
         pool = Pool()
         if not getattr(state_model, '_values', False):
             state_model._values = {}
-        HealthProf = pool.get('gnuhealth.healthprofessional')
-        healthprof = HealthProf.get_health_professional()
+        healthprof_model = pool.get('gnuhealth.healthprofessional')
+        healthprof = healthprof_model.get_health_professional()
         state_model.signed_by = healthprof
         state_model.sign_time = datetime.now()
         return self.transition_save_x()
@@ -174,7 +218,7 @@ class EditComponentWizard(Wizard):
     def transition_save_x(self):
         model = self._component_data['model']
         component = None
-        state_name = 'component'
+        state_name = self._component_data['selected_component']
         if model != 'gnuhealth.encounter':
             compid = self._component_data['active_id']
             component = EncounterComponent.union_unshard(compid)
