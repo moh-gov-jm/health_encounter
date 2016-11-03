ad25510b9fd185cd975794003ae3cb796ab6d61b
Randy1Burrell <Randy1burrell@yahoo.com>
2016-11-02 22:23:18 -0500

    Modified to only run doc tests for the time being

--- tests/test_encounter.py
+++ tests/test_encounter.py
@@ -1,14 +1,21 @@
+"""Automated tests"""
 #!/usr/bin/env python
 
-import sys, os
+import os
+import sys
+import doctest
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import test_view, test_depends 
+                                       # doctest_setup, doctest_teardown)
+
 DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
+                                                    '..', '..', 
+                                                    '..', '..', 
+                                                    '..', 'trytond')))
 if os.path.isdir(DIR):
     sys.path.insert(0, os.path.dirname(DIR))
 
-import unittest
-import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
 
 
 class EncounterViewTestCase(unittest.TestCase):
@@ -32,9 +39,16 @@ class EncounterViewTestCase(unittest.TestCase):
         test_depends()
 
 def suite():
+    """Test suites"""
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        EncounterViewTestCase))
+    # suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+    #     EncounterViewTestCase))
+    suite.addTests(doctest.DocFileSuite('test_encounter.rst',
+                                        setUp=None, 
+                                        tearDown=None, 
+                                        encoding='utf-8', 
+                                        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
+                                        checker=None))
     return suite
 
 if __name__ == '__main__':
